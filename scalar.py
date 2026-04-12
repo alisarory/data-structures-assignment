25ba5bf310cd8aad4b9ef045938936130d6fc41f
alisarory <ali.sarory999@gmail.com>
2026-04-12 18:28:31 +0300

    add scalar file

---
+++ scalar.py
@@ -0,0 +1,17 @@
+# Scalar
+
+x = 5
+print("x =", x)
+print("type of x =", type(x))
+
+y = 3
+print("y =", y)
+print("type of y =", type(y))
+
+print("x + y =", x + y)
+
+x_float = 3.5
+print("x_float =", x_float)
+print("type of x_float =", type(x_float))
+
+print("x_float + y =", x_float + y)
